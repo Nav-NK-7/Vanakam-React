0927183d0df491c139c154c17987e9feae7eb656
Nav-NK-7 <naveen75139@gmail.com>
2026-01-13 13:43:28 +0530

    commited 1

---
+++ app.js
@@ -0,0 +1,7 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hello"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
